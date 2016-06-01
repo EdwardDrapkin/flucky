0490f9b47dd07716411edab9d8edfe534b819f03
Edward Drapkin <edward@colorjar.com>
2016-05-31 21:33:52 -0400

    v1.0.0

--- index.js
+++ index.js
@@ -2,8 +2,17 @@
  * @flow
  */
 
-export FluckyStore from 'lib/FluckyStore.js';
-export FluckyActionCreator from 'lib/FluckyActionCreator';
-export FluckyDispatcher from 'lib/FluckyDispatcher';
-export FluckyComponent from 'lib/FluckyComponent';
-export default Flucky from 'lib/Flucky';
+import React from 'react';
+import _FluckyStore from './lib/FluckyStore.js';
+import _FluckyActionCreator from './lib/FluckyActionCreator';
+import _FluckyDispatcher from './lib/FluckyDispatcher';
+import _FluckyComponent from './lib/FluckyComponent';
+import Flucky from './lib/Flucky';
+
+export default Flucky;
+export const FluckyActionCreator = _FluckyActionCreator;
+export const FluckyDispatch = _FluckyDispatcher;
+export const FluckyComponent = _FluckyComponent;
+export const FluckyStore = _FluckyStore;
+
+
